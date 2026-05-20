7b34dbc5576d7e82737b56a3c4b897e92c6c8be6
darshankardile <darshan.kardil@gmail.com>
2026-05-21 02:32:22 +0530

    api chg

--- app/src/lib/apibasepath.jsx
+++ app/src/lib/apibasepath.jsx
@@ -1,4 +1,4 @@
-const API = "https://cloudfileexplorer-backend.onrender.com/api";
+const API = "https://cloudfileexplorer-backend-1.onrender.com/api";
 // const API="http://localhost:3000/api"
 
 export default API;
